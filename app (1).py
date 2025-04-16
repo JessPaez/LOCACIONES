30cf3827eb57327f0b3e31a243a3cf51ec489c34
JessPaez <chess1018@hotmail.com>
2025-04-16 17:48:25 -0500

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -9,10 +9,12 @@ st.title("📦 Filtrado interactivo de pedidos")
 
 # ⚠️ Reemplazá esta URL con la URL RAW de tu archivo en GitHub
 csv_url = "https://raw.githubusercontent.com/JessPaez/LOCACIONES/refs/heads/main/PEDIDOS_SC.csv"
+csv_url_compras = "https://raw.githubusercontent.com/JessPaez/LOCACIONES/refs/heads/main/COMPRAS.csv"
 
 try:
     # Cargar datos y parsear la columna de fechas directamente
     df = pd.read_csv(csv_url, parse_dates=["Farm Shi"])
+    df_compras = pd.read_cvs(csv_url_compras).fillna("")
 
     # ----------------------------
     # LIMPIEZA Y TRANSFORMACIONES
@@ -45,6 +47,7 @@ try:
             return pd.Series([product_cm, "S.E."])
 
     df[["Product", "Largo"]] = df["Product"].apply(separar)
+    df_compras[["VARIETY","Largo"]] = df_compras["VARIETY"].apply(separar)
 
     # Agrupar
     df = df.groupby(["Farm Shi", "Product", "Cod", "Largo"], as_index=False).sum()
@@ -56,6 +59,9 @@ try:
     # Reordenar columnas para mostrar 'Largo' después de 'Product'
     columnas_ordenadas = ["Farm Shi", "Product", "Largo", "Cod", "Total Units"]
     df = df[columnas_ordenadas]
+    columnas_compras = ['VARIETY', 'Largo' ,'WELY', 'FROS', 'NATIVE', 'GUAISA', 'FLVE', 'ROPR', 'ROSU',
+       'DIND', 'POTR', 'ALIA', 'ATTA', 'DYAN', 'NEVA', 'STRO']
+    df_compras = df_compras[columnas_compras]
 
     # ----------------------------
     # FILTROS
@@ -65,16 +71,15 @@ try:
     fecha_filtro = st.sidebar.date_input("📅 Fecha exacta:")
     variedad_filtro = st.sidebar.text_input("🌸 Variedad :", value="")
     cod_opcion = st.sidebar.selectbox("🏷️ Código:", options=["AMBOS", "FDB", "LOC"])
-
-    variedad_compras_filtro = st.sidebar.text_input('🌸 Variedad compras :',value="")
-
     filtro = df.copy()
+    filtro_compras = df_compras.copy()
 
     if fecha_filtro:
         filtro = filtro[filtro["Farm Shi"] == fecha_filtro]
 
     if variedad_filtro:
         filtro = filtro[filtro["Product"].str.contains(variedad_filtro, case=False, na=False)]
+        filtro_compras = filtro_compras[filtro_compras["VARIETY"].str.contains(variedad_filtro, case=False, na=False)]
 
     if cod_opcion != "AMBOS":
         filtro = filtro[filtro["Cod"] == cod_opcion]
@@ -88,6 +93,7 @@ try:
         st.warning("No hay resultados para los filtros seleccionados.")
     else:
         st.dataframe(filtro)
+        st.datafrmae(filtro_compras)
 
 except Exception as e:
     st.error(f"❌ Error al cargar el archivo desde GitHub: {e}")
